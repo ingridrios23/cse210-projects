190f23fbe3393f9ab8f2c41551e4d2f98cd207e7
ingridrios23 <carolingrid.232o@gmail.com>
2025-05-07 18:46:36 -0400

    added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //ask the user for their name.
+        Console.Write("what is your frist name?");
+         string first = Console.ReadLine();
+
+         Console.Write("what is your last name?");
+         string last =Console.ReadLine();
+
+         Console.WriteLine($"your name is {last}, {first} {last}");
     }
 }
