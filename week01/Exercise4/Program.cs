321f7240dd1f0fa60e02d982949138b812561f27
ingridrios23 <carolingrid.232o@gmail.com>
2025-05-10 17:33:46 -0400

    add a comment

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        
+        int sum = numbers.Sum();
+        double average = (double)sum / numbers.Count;
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+       
+        int smallestPositive = numbers
+            .Where(n => n > 0)
+            .DefaultIfEmpty() 
+            .Min();
+
+        if (smallestPositive > 0)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+
+        
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
     }
 }
